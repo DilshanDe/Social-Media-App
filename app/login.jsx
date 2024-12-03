aa744be196c1ca5a9ed98ef804d4ff7a40cce7f8
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-03 06:42:12 +0530

    Update login.jsx

--- app/login.jsx
+++ app/login.jsx
@@ -1,5 +1,5 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
 
 import Icon from '../assets/icons'
@@ -7,11 +7,40 @@ import BackButton from '../components/BackButton'
 import { useRouter } from 'expo-router'
 import{hp,wp}from'../helpers/comman'
 import { theme } from '../constants/theme'
+import { TextInput } from 'react-native'
+import Input from '../components/Input'
+
+import Button from'../components/Button'
+import { Pressable } from 'react-native'
+import { supabase } from '../lib/supabase'
+
 
 const Login = () => {
   const router=useRouter();
+  const emailRef=useRef('');
+  const passwordRef=useRef('');
+  const[loading,setLoading]=useState(false);
+  const onSubmit=async()=>{
+    if(!emailRef.current || !passwordRef.current){
+      Alert.alert('Login',"Please Fill the All Fields");
+      return;
+    }
+    let email=emailRef.current.trim();
+    let password=passwordRef.current.trim();
+    setLoading(true);
+    const{error}=await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+    setLoading(false);
+    console.log('error',error);
+    if(error){
+      Alert.alert('Login',error.message);
+    }
+
+  }
   return (
-    <ScreenWrapper>
+    <ScreenWrapper bg='white'>
       <StatusBar style='dark'/>
       <View style={styles.container}>
         <BackButton router={router}/>
@@ -23,6 +52,36 @@ const Login = () => {
             <Text style={styles.welcomeText}>Welcome Back</Text>
           </View>
           {/*form*/}
+          <View style={styles.form}>
+            <Text style={{fontSize:hp(1.5),color:theme.colors.text}}>
+              Please Login to Continue
+            </Text>
+            <Input
+            icon={<Icon name='mail' size={26}strokeWidth={1.6}/>}
+            placeholder='Enter your Email'
+            onChangeText={value=>emailRef.current= value}
+            />
+            <Input
+            icon={<Icon name='lock' size={26}strokeWidth={1.6}/>}
+            placeholder='Enter your Password'
+            secureTextEntry
+            onChangeText={value=>passwordRef.current= value}
+            />
+            <Text style={styles.forgotPassword}>
+              Forgot password?
+            </Text>
+            {/*button*/}
+            <Button title={'Login'} loading={loading} onPress={onSubmit}/>
+          </View>
+          {/*footer*/}
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>
+              Don't have an Account?
+            </Text>
+            <Pressable onPress={()=>router.push('signUp')}>
+              <Text style={[styles.footerText,{color:theme.colors.primaryDark,fontWeight:theme.fonts.semibold}]}>Sign Up</Text>
+            </Pressable>
+          </View>
       </View>
 
     </ScreenWrapper>
@@ -41,7 +100,29 @@ const styles = StyleSheet.create({
     fontSize:hp(4),
     fontWeight:theme.fonts.bold,
     color:theme.colors.text
+  },
+  form:{
+    gap:25,
+
+  },
+  forgotPassword:{
+    textAlign:'right',
+    fontWeight:theme.fonts.semibold,
+    color:theme.colors.text
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  footerText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6),
   }
+  
+
 
 
 })
