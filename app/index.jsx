4f7c55321bc023d0c3553bb0df09b70e1ebb209c
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-03 06:42:09 +0530

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Text, Button, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Loading from '../components/Loading';
 
 export default function Index() {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <StatusBar barStyle="dark-content" backgroundColor="white" />
-      <Text>Index</Text>
-      <Button title="Welcome" onPress={() => navigation.navigate('welcome')} />
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Loading/>
+      
     </View>
   );
 }
