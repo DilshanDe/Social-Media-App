5688397d3517dc855a7654a464d7a57f155a0fe7
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-08 07:44:33 +0530

    Update postService.js

--- Services/postService.js
+++ Services/postService.js
@@ -31,3 +31,26 @@ export const createOrUpdatePost= async(post)=>{
         return{success:false,msg:"could not create your post"};
     }
 }
+
+export const fetchPosts = async (limit = 10) => {
+    try {
+        const { data, error } = await supabase
+            .from('posts')
+            .select(`
+                *,
+                users(id ,name,image)
+                `)
+            .order('created_at', { ascending: false }) 
+            .limit(limit);
+
+        if (error) {
+            console.log('fetchPosts error', error);
+            return { success: false, msg: "Could not fetch your posts" };
+        }
+
+        return { success: true, data: data };
+    } catch (error) {
+        console.log('fetchPosts error', error);
+        return { success: false, msg: "Could not fetch your posts" };
+    }
+};
