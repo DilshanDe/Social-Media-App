307bdb1d2bd113d18363902dff8199ee9b6bf7c3
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-09 07:30:25 +0530

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Loading from '../components/Loading';
 
@@ -7,9 +7,11 @@ export default function Index() {
   const navigation = useNavigation();
 
   return (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-      <Loading/>
-      
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* Add StatusBar with dark content */}
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      <Loading />
     </View>
   );
 }
+
