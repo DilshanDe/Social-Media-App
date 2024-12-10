9988559e1fb555f3a738f0677832316aab7ea9cf
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-10 07:11:03 +0530

    Update postService.js

--- Services/postService.js
+++ Services/postService.js
@@ -38,7 +38,8 @@ export const fetchPosts = async (limit = 10) => {
             .from('posts')
             .select(`
                 *,
-               user: users(id ,name,image)
+               user: users(id ,name,image),
+               postLikes(*)
                 `)
             .order('created_at', { ascending: false }) 
             .limit(limit);
@@ -48,9 +49,47 @@ export const fetchPosts = async (limit = 10) => {
             return { success: false, msg: "Could not fetch your posts" };
         }
 
-        return { success: true, data: data };
+        return { success: true, data:data};
     } catch (error) {
         console.log('fetchPosts error', error);
         return { success: false, msg: "Could not fetch your posts" };
+    }
+};
+export const createPostLike = async (postLike) => {
+    try {
+        const{data,error}=await supabase
+        .from('postLikes')
+        .insert(postLike)
+        .select()
+        .single();
+       
+        if (error) {
+            console.log('postLike error', error);
+            return { success: false, msg: "could not like the post" };
+        }
+
+        return { success: true, data: data };
+    } catch (error) {
+        console.log('fetchPosts error', error);
+        return { success: false, msg: "Could not like the  posts" };
+    }
+};
+export const removePostLike = async (postId,userId) => {
+    try {
+        const{error}=await supabase
+        .from('postLikes')
+        .delete()
+        .eq('userId',userId)
+        .eq('postId',postId)
+       
+        if (error) {
+            console.log('postLike error', error);
+            return { success: false, msg: "could not remove the postLike" };
+        }
+
+        return { success: true,};
+    } catch (error) {
+        console.log('fetchPosts error', error);
+        return { success: false, msg: "Could not remove postsLike" };
     }
 };
