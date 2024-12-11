beaa58b72e835ea669c15cb19338fe5095849c14
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-11 15:16:04 +0530

    Create CommentItem.jsx

---
+++ components/CommentItem.jsx
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { theme } from '../constants/theme'
+import { hp,wp } from '../helpers/comman'
+import Avatar from './Avatar'
+
+const CommentItem = ({
+    item
+}) => {
+  return (
+    <View style={styles.container}>
+      <Avatar
+      uri={item?.user?.image}
+      />
+      <View style={styles.content}>
+        <View style={{flexDirection:'row', justifyContent:"space-between",alignItems:'center'}}>
+            
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default CommentItem
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        gap: 7,
+      },
+      content: {
+        backgroundColor: 'rgba(0,0,0,0.06)',
+        flex: 1,
+        gap: 5,
+        paddingHorizontal: 14,
+        paddingVertical: 10,
+        borderRadius: theme.radius.md,
+        borderCurve: 'continuous',
+      },
+      highlight: {
+        borderWidth: 0.2,
+        backgroundColor: 'white',
+        borderColor: theme.colors.dark,
+        shadowColor: theme.colors.dark,
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
+        elevation: 5,
+      },
+      nameContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 3,
+      },
+      text: {
+        fontSize: hp(1.6),
+        fontWeight: theme.fonts.medium,
+        color: theme.colors.textDark,
+      },
+})
