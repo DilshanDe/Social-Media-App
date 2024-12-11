067ee6198deb09ea72eb3939cfc3d1b12a523523
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-11 08:47:50 +0530

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -44,7 +44,14 @@ const MainLayout= () => {
       screenOptions={{
         headerShown: false,
       }}
-    />
+    >
+      <Stack.Screen
+      name='(main)/postDetails'
+      options={{
+        presentation:'modal'
+      }}
+      />
+    </Stack>
   );
 };
 
