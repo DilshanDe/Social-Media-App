441c625691dbaa0042b9861be4ab8291b2001783
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-11 09:08:48 +0530

    Update postDetails.jsx

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { useEffect } from 'react';
 import { fetchPostsDetails } from '../../Services/postService';
+import { hp, wp } from '../../helpers/comman';
+import { theme } from '../../constants/theme';
 
 const PostDetails = () => {
    const{postId}= useLocalSearchParams();
@@ -18,6 +20,7 @@ const PostDetails = () => {
    const getPostDetails=async()=>{
     //fetch post detais
     let res =await fetchPostsDetails(postId);
+    if(res.success) setPost(res.data);
    }
   return (
     <View>
@@ -28,4 +31,45 @@ const PostDetails = () => {
 
 export default PostDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingVertical: wp(7),
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  list: {
+    paddingHorizontal: wp(4),
+  },
+  sendIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.8,
+    borderColor: theme.colors.primary,
+    borderRadius: theme.radius.lg,
+    borderCurve: 'continuous',
+    height: hp(5.8),
+    width: hp(5.8),
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  notFound: {
+    fontSize: hp(2.5),
+    color: theme.colors.text,
+    fontWeight: theme.fonts.medium,
+  },
+  loading: {
+    height: hp(5.8),
+    width: hp(5.8),
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: [{ scale: 1.3 }],
+  },
+})
