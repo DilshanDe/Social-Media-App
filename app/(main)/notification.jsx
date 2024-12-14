5ab2f4ee36440d68a19b0232ea8f3dbe4614ee69
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-14 12:47:08 +0530

    Update notification.jsx

--- app/(main)/notification.jsx
+++ app/(main)/notification.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { fetchNotifications } from '../../Services/notificationService';
 import { useAuth } from '../../context/AuthContext';
+import { hp, wp } from '../../helpers/comman';
+import ScreenWrapper from '../../components/ScreenWrapper';
+import { ScrollView } from 'react-native';
 
 const Notification = () => {
   const[notificatins,setNotifications]=useState([]);
@@ -15,15 +18,35 @@ const Notification = () => {
 
   const getNotifications= async()=>{
     let res = await fetchNotifications(user.id);
-    console.log("notifications:",res);
+    if(res.success) setNotifications(res.data);
   }
   return (
-    <View>
-      <Text>Notification</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.listStyle}>
+          
+        </ScrollView>
+      </View>
+    </ScreenWrapper>
   )
 }
 
 export default Notification
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(4),
+  },
+  listStyle: {
+    paddingVertical: 20,
+    gap: 10,
+  },
+  noData: {
+    fontSize: hp(1.8),
+    fontWeight: '500',
+    color: '#333333',
+    textAlign: 'center',
+  },
+  
+})
