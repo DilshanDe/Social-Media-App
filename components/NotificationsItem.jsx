3a71b166c1a3069b183c1e227b1224667a449108
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-15 19:02:46 +0530

    Update NotificationsItem.jsx

--- components/NotificationsItem.jsx
+++ components/NotificationsItem.jsx
@@ -1,14 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { theme } from '../constants/theme'
+import { hp } from '../helpers/comman'
+import { TouchableOpacity } from 'react-native'
+import Avatar from './Avatar'
 
-const NotificationsItem = () => {
+const NotificationsItem = ({
+  item,
+  router
+}) => {
+  const handleClick=()=>{
+    // open post details
+  }
+  console.log('item',item);
   return (
-    <View>
-      <Text>NotificationsItem</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleClick}>
+      {/*<Avatar/>*/}
+    </TouchableOpacity>
   )
 }
 
 export default NotificationsItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 12,
+    backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderColor: theme.colors.darkLight,
+    padding: 15,
+    // paddingVertical: 12,
+    borderRadius: theme.radius.xxl,
+    borderCurve: 'continuous'
+},
+nameTitle: {
+    flex: 1,
+    gap: 2,
+},
+text: {
+    fontSize: hp(1.6),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.text,
+}
+
+})
