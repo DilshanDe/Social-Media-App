0c65b3ea3b692a3cdc6c6a9dc2bcd4a3017cbb0d
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-12-15 18:47:15 +0530

    Create NotificationsItem.jsx

---
+++ components/NotificationsItem.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NotificationsItem = () => {
+  return (
+    <View>
+      <Text>NotificationsItem</Text>
+    </View>
+  )
+}
+
+export default NotificationsItem
+
+const styles = StyleSheet.create({})
